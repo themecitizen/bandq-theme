4e11d9c8632baac829a29fd2c5edd90d05754e78
TaiDN <taidongoc@gmail.com>
2020-11-23 01:22:46 +0700

    - completed footer bottom

--- templates/footers/layout-1.php
+++ templates/footers/layout-1.php
@@ -1,13 +1,13 @@
 <div class="footer-bottom">
 	<div class="container">
-		<div class="row align-items-center">
+		<div class="row align-items-center justify-content-center">
                 <span>© 2020 Braddock and Purcell</span>
                 <?php
                 if (has_nav_menu('footer')) {
                     wp_nav_menu(array(
                         'theme_location' => 'footer',
                         'container' => false,
-                        'menu_class' => 'nav-menu navbar-nav menu'
+                        'menu_class' => 'menu d-flex'
                     ));
                 }
                 ?>
