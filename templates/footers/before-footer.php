0ae58f0da936aa834dcfd965172b52dc94d20045
TaiDN <taidongoc@gmail.com>
2020-11-24 01:54:11 +0700

    - completed footer

--- templates/footers/before-footer.php
+++ templates/footers/before-footer.php
@@ -4,13 +4,23 @@
             <div class="col-12">
                 <h3>CONTACT US</h3>
                 <h4>Send us a message</h4>
-
                 <div class="contact-form">
                 <?php
                 echo do_shortcode('[contact-form-7 title="Footer-Contact"]');
                 ?>
                 </div>
-            </div>
+             </div>
+        </div>
+    </div>
+    <div class="social-section">
+        <div class="container">
+            <ul class="socials-info">
+                <li><img title="phone" src="<?php echo BANDQ_URL . '/img/phone-green.png'; ?>" alt="<?php echo esc_attr__( 'phone', 'bandq' ); ?>">212-715-0899</li>
+                <li><img title="email" src="<?php echo BANDQ_URL . '/img/mail-green.png'; ?>" alt="<?php echo esc_attr__( 'email', 'bandq' ); ?>"> kathy@bandp.com</li>
+                <li><img title="email" src="<?php echo BANDQ_URL . '/img/mail-green.png'; ?>" alt="<?php echo esc_attr__( 'email', 'bandq' ); ?>"> paul@bandp.com</li>                
+                <li><img title="facebook" src="<?php echo BANDQ_URL . '/img/facebook-green.png'; ?>" alt="<?php echo esc_attr__( 'facebook', 'bandq' ); ?>"> braddockandpurcel</li>
+                <li><img title="instagram" src="<?php echo BANDQ_URL . '/img/instagram-green.png'; ?>" alt="<?php echo esc_attr__( 'instagram', 'bandq' ); ?>"> braddock_and_purcel</li>
+            </ul>
         </div>
     </div>
 </div>
