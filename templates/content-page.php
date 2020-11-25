ace4f09bdb83a15e44cfdf5c70dd8a95a1be99e1
duybui <bduy1789@gmail.com>
2020-11-26 01:40:31 +0700

    remove entry header

--- templates/content-page.php
+++ templates/content-page.php
@@ -13,7 +13,6 @@
 	<div class="entry-content">
 		<?php
 			the_content();
-
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bandq' ),
 				'after'  => '</div>',
